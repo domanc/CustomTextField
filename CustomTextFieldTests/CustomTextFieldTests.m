46566ddd6cc75e56e61bb2c82cb7b9493bbe44d8
Doman <chendianming@chendianmingdeMacBook-Pro.local>
2017-06-13 16:05:50 +0800

    Initial Commit

---
+++ CustomTextFieldTests/CustomTextFieldTests.m
@@ -0,0 +1,39 @@
+//
+//  CustomTextFieldTests.m
+//  CustomTextFieldTests
+//
+//  Created by Doman on 2017/6/13.
+//  Copyright © 2017年 Doman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomTextFieldTests : XCTestCase
+
+@end
+
+@implementation CustomTextFieldTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
